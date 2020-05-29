5e6c1e58ca5e363fc81c3ce9af76292d499ba94b
Dell <1419517126@qq.com>
2020-05-29 14:37:02 +0800

    重写，完成

---
+++ bwShop/settings.py
@@ -0,0 +1,167 @@
+import os
+import sys
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,BASE_DIR)
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
+sys.path.insert(0,os.path.join(BASE_DIR,'extra_apps'))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '7$m1!j&%)oh5_(n72#mw#pcnm*+dhv1(voni_#f6+#(^%^1*3g'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+#重载系统的用户，让UserProfile生效
+AUTH_USER_MODEL = 'users.UserProfile'
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'users',
+    'goods',
+    'trade',
+    'user_operation',
+    'xadmin',
+    'crispy_forms',
+    'django_filters',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'coreschema',
+    'DjangoUeditor'
+
+]
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE':10,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    )
+}
+
+AUTHENTICATION_BACKENDS = (
+    'users.views.CustomBackend',
+)
+
+import datetime
+#有效期限
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+}
+APPEND_SLASH=False
+# 手机号码正则表达式
+REGEX_MOBILE = "^1[358]\d{9}$|^147\d{8}$|^176\d{8}$"
+# 云片网 api_key
+APIKEY = '9125dde81bd0553fde56fb1db6177193'
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+ROOT_URLCONF = 'bwShop.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'bwShop.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shop',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST':'127.0.0.1',
+        'OPTIONS':{
+            'init_command':'SET default_storage_engine=INNODB;'
+        }
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = 'zh-hans'
+
+TIME_ZONE = 'Asia/Shanghai'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# 上传图片
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
---
+++ bwShop/urls.py
@@ -0,0 +1,46 @@
+from django.urls import path,include
+from django.views.static import serve
+
+import xadmin
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+from rest_framework import routers
+from rest_framework.authtoken import views
+from rest_framework_jwt.views import obtain_jwt_token
+
+from bwShop.settings import MEDIA_ROOT
+from goods.views import GoodsListViewSet, CategoryViewSet, BannerViewset, IndexCategoryViewset
+from users.views import SmsCodeViewset,UserViewset
+from user_operation.views import UserFavViewset, LeavingMessageViewset, AddressViewset
+from trade.views import ShoppingCartViewset,OrderViewset
+
+router = routers.DefaultRouter()
+router.register('goods',GoodsListViewSet)
+router.register('categorys',CategoryViewSet,basename='categorys')
+router.register('code',SmsCodeViewset,basename='code')
+router.register('users',UserViewset,basename="users")
+router.register('userfavs',UserFavViewset,basename='userfavs')
+router.register('messages',LeavingMessageViewset,basename='messages')
+router.register('address',AddressViewset , basename="address")
+router.register('shopcarts',ShoppingCartViewset,basename='shopcarts')
+router.register('orders',OrderViewset,basename='orders')
+router.register('banners',BannerViewset,basename='banners')
+router.register('indexgoods',IndexCategoryViewset,basename='indexgoods')
+
+
+schema_view = get_schema_view(title='corejson')
+
+urlpatterns = [
+    path('xadmin/',xadmin.site.urls),
+    path('ueditor/',include('DjangoUeditor.urls')),
+    # 文件上传路径
+    path('media/<path:path>',serve,{'document_root':MEDIA_ROOT}),
+    # path('goods/',GoodsListViewSet.as_view(),name='goods-list'),
+    path(r'',include(router.urls)),
+    path('api-token-auth',views.obtain_auth_token),
+    # path('jwt-auth', obtain_jwt_token ),
+    path('login/', obtain_jwt_token ),
+    path('api-auth/',include('rest_framework.urls')),
+    path('docs/',include_docs_urls(title='生鲜项目的文档')),
+    path('schema/',schema_view)
+]
